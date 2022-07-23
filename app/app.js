dda7c17479ce2bc20ccb78750a2b8ed6b59be8d0
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2022-07-23 14:02:02 +0900

    src dir 최적화, app view dir 변경, nodemon(auto restart) 추가

--- app/app.js
+++ app/app.js
@@ -3,11 +3,12 @@ const express = require("express");
 const app = express();
 
 // 라우팅
-const home = require("./routes/home")
+const home = require("./src/routes/home")
 
 //view app setting;
-app.set("views", "./views"); // view파일은 views폴더에 작성
+app.set("views", "./src/views"); // view파일은 views폴더에 작성
 app.set("view engine", "ejs"); // view engine은 ejs
+app.use(express.static(`${__dirname}/src/public`)) // 경로 사용
 
 
 app.use("/", home) // 미들웨어를 등록하는 매소드 -> 라우트 개념
