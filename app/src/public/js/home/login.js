7b42490dae329026d86ac99f9f6ad1213193492d
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2022-07-23 16:21:44 +0900

    login.,..

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -17,7 +17,13 @@ const login = () => {
             "Content-Type":"application/json"
         },
         body: JSON.stringify(req)
-    }).then(async (res) => res.json()).then(console.log)
+    }).then(async (res) => res.json()).then((rs) => {
+        if(rs.success) {
+          location.href= '/' ;
+        } else {
+            alert('로그인 실패')
+        }
+    }).catch((err) => console.error(new Error('로그인 중 에러 발생')))
 }
 
 btn.addEventListener('click', login);
