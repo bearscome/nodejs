7e3f6a63b58795c37cca34dfafb03e073b87501f
DESKTOP-6HDAURQ\gytjd <dlgytjdsla@naver.com>
2022-07-24 17:46:00 +0900

    aws

--- app/src/routes/home/index.js
+++ app/src/routes/home/index.js
@@ -14,3 +14,12 @@ router.post("/register", process.register);
 
 module.exports = router;
 // javascript의 export와 동일한듯
+
+// aws 접속
+/**
+ * CMD -> mysql -h "myendpoint" -u "userId(admin)" -p
+ * password : password
+ * show mydatabases
+ * https://www.youtube.com/watch?v=C7LK20tlQ6Y&list=PLSK4WsJ8JS4cQ-niGNum4bkK_THHOizTs&index=32 (30번 동영상)
+ * aws 세팅 방법
+ */
